b108f08b14dee2775dd9e6cd840a2790d90c1578
Anant Naithani <anant7738w@gmail.com>
2026-09-13 19:11:46 +0530

    rehersal 2

--- main.py
+++ main.py
@@ -17,8 +17,18 @@ def niggatron():
 button = tkinter.Button(text="change", command=niggatron)
 button.pack()
 
+def listbox_item(event):
+    label.config(text=letterbox.get(letterbox.curselection()))
+    print(letterbox.get(letterbox.curselection()))
 
+Attribute = tkinter.IntVar()
+letterbox = tkinter.Listbox(height=5)
+stuff = ["apple", "orange", "banana", "cherry", "tangerine"]
+for i in stuff:
+    letterbox.insert(stuff.index(i), i)
 
+letterbox.bind("<<ListboxSelect>>",listbox_item)
+letterbox.pack()
 
 
 
